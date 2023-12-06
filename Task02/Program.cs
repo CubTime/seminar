ee4673e6c9ce8a286ae57a9b6e22e10ab58567e5
missath <aiguys.ru@gmail.com>
2023-12-07 00:10:37 +0100

    Add Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите корень: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите корень этого числа: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+
+if (x1 == x2 * x2) Console.WriteLine($"{x2} x {x2} = {x1}, True!");
+else if (x1 != x2 * x2) Console.WriteLine($"{x2} x {x2} ≠ {x1}, False!");
+else Console.WriteLine("Вы ввели неверное число!");
