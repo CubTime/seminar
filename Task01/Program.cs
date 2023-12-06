fb2a8fb0f161a048b11165c4da48b2f2b6a47cac
missath <aiguys.ru@gmail.com>
2023-12-06 22:46:41 +0100

    Add Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите целое число: ");
+double number = Convert.ToInt32(Console.ReadLine());
+double square = number * number;
+Console.Write($"Квадрат числа {number} = {square}");
